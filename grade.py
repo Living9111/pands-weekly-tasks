a0036fd2d5ac79bfd07de1538821b591185f1abb
Livingstone Atmakur <g00472968@atu.ie>
2025-04-24 18:21:58 +0100

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,13 @@
+percentage= float(input("enter the percentage:"))
+if percentage < 0 or percentage >100:
+    print("invalid percentage, please enter a number between 0 and 100")
+elif percentage < 40:
+    print("fail")
+elif percentage<50:
+    print("pass")
+elif percentage<60:
+    print("Merit 2")
+elif percentage<70:
+    print("Merit 1")
+else: 
+    print("Distinction")
