396b791d3c779add814bce7d011510cdd26718fb
Livingstone Atmakur <g00472968@atu.ie>
2025-04-24 10:32:47 +0100

    Add files via upload

--- testTypes.py
+++ testTypes.py
@@ -1,1 +1,19 @@
+i=3
+fl=3.5
+isa=True
+memo=" how are you"
+lots= []
+
+print(i)
+print(fl)
+print(isa)
+print(memo)
+print(lots)
+
+
+print(type(i))
+print(type(fl))
+print(type(isa))
+print(type(memo))
+print(type(lots))
 
