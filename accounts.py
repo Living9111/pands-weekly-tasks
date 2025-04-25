78d612bfbb4f72c69606be930f2f2b96fd968d78
Livingstone Atmakur <g00472968@atu.ie>
2025-04-25 09:38:36 +0100

    Update accounts.py

--- accounts.py
+++ accounts.py
@@ -3,3 +3,4 @@ account = user[-4:].rjust(len(user), 'X')
 print("your account number is: ",account)
 
 
+# https://stackoverflow.com/questions/45229343/program-in-python-that-will-prompt-the-user-to-enter-an-account-number-consists
