480a949ecdaeb975508f4517240eacdd1fe677df
Livingstone Atmakur <g00472968@atu.ie>
2025-04-26 14:46:22 +0100

    Add files via upload

---
+++ doAdd(students).py
@@ -0,0 +1,11 @@
+students= []
+def readModules():
+ return []
+def doAdd(students):
+ currentStudent = {}
+ currentStudent["name"]=input("enter name :")
+ currentStudent["modules"]= readModules()
+ students.append(currentStudent)
+doAdd(students)
+doAdd(students)
+print (students)
