3f2bce614126a08488c259c29eeb7f772fe43b16
Livingstone Atmakur <g00472968@atu.ie>
2025-04-26 17:36:24 +0100

    Add files via upload

---
+++ dict_json.py
@@ -0,0 +1,7 @@
+import json
+FILENAME=r"C:\Users\MyPC\Desktop\week 1\week 2\testdict.json"
+sample= dict(name='fred', age=31, grades=[1,34,55])
+def writeDict(obj):
+ with open(FILENAME, 'wt') as f:
+  json.dump(obj,f)
+writeDict(sample)
