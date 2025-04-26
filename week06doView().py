50504a84a7dff192dc4f319f4a39eda1ce814efd
Livingstone Atmakur <g00472968@atu.ie>
2025-04-26 15:14:25 +0100

    Add files via upload

---
+++ week06doView().py
@@ -0,0 +1,9 @@
+def displayModules(modules):
+    print("\tName \tGrade")
+    for module in modules:
+        print(f"\t{module['name']} \t{module['grade']}")
+
+def doView(students):
+    for currentStudent in students:
+        print(currentStudent["name"])
+        displayModules(currentStudent["modules"])
