feddf0ee9638992580a900baf3aa0946cdea03dc
Livingstone Atmakur <g00472968@atu.ie>
2025-04-26 17:26:54 +0100

    Add files via upload

--- count_wk7.py
+++ count_wk7.py
@@ -1,3 +1,4 @@
+#a)
 FILENAME = r"C:\Users\MyPC\Desktop\week 1\week 2\count.txt"
 def readNumber():
  with open(FILENAME) as f:
@@ -5,3 +6,27 @@ def readNumber():
  return number
 num = readNumber()
 print ('count text file contains:',num)
+
+#b)
+#FILENAME = "C:\Users\MyPC\Desktop\week 1\week 2\count.txt"
+def writeNumber(number):
+ with open(FILENAME, "wt") as f:
+  f.write(str(number))
+# test it
+writeNumber(3)
+
+#c)
+FILENAME = r"C:\Users\MyPC\Desktop\week 1\week 2\count.txt"
+def readNumber():
+ with open(FILENAME) as f:
+  number = int(f.read())
+ return number
+def writeNumber(number):
+ with open(FILENAME, "wt") as f:
+  f.write(str(number))
+ num = readNumber()
+ num += 1
+ print (f"we have run this program {num} times")
+writeNumber(num)
+
+#d)
