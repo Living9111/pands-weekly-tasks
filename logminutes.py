dfbd54a9a8d2196208c178f98352a5659e29bd80
Livingstone Atmakur <g00472968@atu.ie>
2025-04-27 18:47:53 +0100

    Add files via upload

---
+++ logminutes.py
@@ -0,0 +1,36 @@
+from timesheetentry import Timesheetentry
+import datetime as dt
+
+class Employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        self.timesheets = []
+
+    def __str__(self):
+        return self.first + ' ' + self.last
+
+    def logminutes(self, project, minutes):
+        now = dt.datetime.now()
+        timesheetentry = Timesheetentry(project, now, minutes)
+        self.timesheets.append(timesheetentry)
+
+    def gettotaltime(self):
+        total_minutes = 0
+        for ts in self.timesheets:
+            total_minutes += ts.duration
+        return total_minutes
+
+if __name__ == '__main__':
+    test = Employee('some', 'one')
+    print(test)
+    assert ('some one' == str(test))
+
+    test.logminutes('p1', 30)
+    test.logminutes('p1', 60)
+
+    mins = test.gettotaltime()
+    print(mins)
+    assert (mins == 90)
+
+    print('all good')
