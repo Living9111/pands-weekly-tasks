6dfa5d48f9aadda15f626d239d6ba7482cc546b9
Livingstone Atmakur <g00472968@atu.ie>
2025-04-27 18:32:54 +0100

    Add files via upload

---
+++ (in employee.py
@@ -0,0 +1,13 @@
+class Employee:
+ timesheets = []
+ def __init__(self, first, last):
+  self.first = first
+  self.last = last
+
+ def __str__(self):
+  return self.first + ' ' + self.last
+
+if __name__ == '__main__':
+ test = Employee('some', 'one')
+ print (test)
+ assert ( 'some one' == str(test))
